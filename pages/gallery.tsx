6038450015f936cd2f71ca06eb434e684e88950b
Erick Christian <erickchristian48@gmail.com>
2022-04-30 10:22:02 +0700

    feat: placeholder pages

---
+++ pages/gallery.tsx
@@ -0,0 +1,56 @@
+import { Icon } from '@iconify/react'
+import { Box, Button, createStyles, Text, Title } from '@mantine/core'
+import { NextPage } from 'next'
+import about from '~/assets/about.webp'
+import Footer from '~/components/Footer'
+import Header from '~/components/Header'
+import Article from '~/components/Home/Article'
+import Nav from '~/components/Nav'
+
+const useStyles = createStyles((theme) => ({}))
+
+const Gallery: NextPage = () => {
+  const { classes } = useStyles()
+
+  return (
+    <>
+      <Nav />
+      <Header />
+
+      <Box component="main">
+        <Article imageSrc={about.src} withDivider>
+          <Title order={2} my="md">
+            Tentang Kami
+          </Title>
+          <Text mb="md">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Repellendus voluptas doloremque enim quos adipisci dolore non
+            reiciendis iste, nam fugiat, modi voluptatem magni. Totam at eos
+            quaerat accusamus, qui aliquam.
+          </Text>
+          <Button rightIcon={<Icon icon="carbon:arrow-right" />}>
+            Selengkapnya
+          </Button>
+        </Article>
+        <Article imageSrc={about.src} textOnRight>
+          <Title order={2} my="md">
+            Tentang Kami
+          </Title>
+          <Text mb="md">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Repellendus voluptas doloremque enim quos adipisci dolore non
+            reiciendis iste, nam fugiat, modi voluptatem magni. Totam at eos
+            quaerat accusamus, qui aliquam.
+          </Text>
+          <Button rightIcon={<Icon icon="carbon:arrow-right" />}>
+            Selengkapnya
+          </Button>
+        </Article>
+      </Box>
+
+      <Footer />
+    </>
+  )
+}
+
+export default Gallery
