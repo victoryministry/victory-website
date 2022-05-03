f5cb5509df51426964af62d1686a414282037598
Erick Christian <erickchristian48@gmail.com>
2022-05-03 18:16:00 +0700

    feat: add nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,37 @@
+import { MantineProvider } from '@mantine/core'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-import { MantineProvider } from '@mantine/core'
+import { useRouter } from 'next/router'
+import nProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import { useEffect } from 'react'
 import { RecoilRoot } from 'recoil'
 import { theme } from '~/mantine.theme'
 
+nProgress.configure({ showSpinner: false })
+
 export default function App(props: AppProps) {
   const { Component, pageProps } = props
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = () => {
+      nProgress.start()
+    }
+    const handleStop = () => {
+      nProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router.events])
 
   return (
     <>
